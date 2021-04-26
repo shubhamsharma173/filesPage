cdca8fe5fc7630e4533c9ac9ff4812ba7406b4e4
SHOW <56506832+shubhamsharma173@users.noreply.github.com>
2021-04-26 17:04:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,24 +8,20 @@ app.use(express.static("public"));
 
 const testFolder = "./files/";
 let files = [];
+fs.readdirSync(testFolder).forEach((file) => {
+  //   console.log(file);
+  files.push(file);
+});
 app.get("/", (req, res) => {
-  const list = async (testFolder) => {
-    fs.readdirSync(testFolder).forEach((file) => {
-      //   console.log(file);
-      files.push(file);
-    });
-  };
-  list(testFolder).then(() => {
-    // console.log(files);
-    res.render("page", { list: files });
-  });
+  // console.log(files);
+  res.render("page", { list: files });
 });
 
 app.get("/download/:name", function (req, res) {
-  const file = `${__dirname}/files/`+ req.params.name;
+  const file = `${__dirname}/files/` + req.params.name;
   res.download(file); // Set disposition and send it.
 });
 
-app.listen(process.env.PORT ||port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
